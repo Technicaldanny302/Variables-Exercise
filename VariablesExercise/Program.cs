5b5bf718def0fa82465ca705712b31e22b2a6892
Dan Marino <technicaldanny302@gmail.com>
2024-06-29 20:15:01 -0700

    finished exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string customerName = ("micheal");
+            Console.WriteLine($"Hey my names {customerName},how's your day going?");
+
+            int theNum = 87;
+            Console.WriteLine($"hey Janet you toatal will be {theNum}");
+
+            char x = 'A';
+            Console.WriteLine("whats up, i got mike! yeah i got a {x} in physics");
+
+            bool Questions = false;
+            Console.WriteLine("I heard you did'nt take take the trash out today");
+            Console.WriteLine(Questions);
+
+            double thatNum = 100.32;
+            Console.WriteLine($"Hey, how was my work today?");
+            Console.WriteLine($"it was {thatNum}");
+
+            decimal percentage = 342.01m;
+            Console.WriteLine($"whats my total return for that stake");
+            Console.WriteLine($"It was {percentage}");
         }
     }
 }
